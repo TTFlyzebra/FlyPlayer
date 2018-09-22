be198e05e06d04c3f96d395b2700508fce51f755
tangshiyuan <tang.shiyuan@jancar.com.cn>
2018-09-22 11:50:42 +0800

    代码整理

--- jancarplayer/Android.mk
+++ jancarplayer/Android.mk
@@ -1,16 +1,27 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES :=  glide-3.7.0:libs/glide-3.7.0.jar \
+                                         disklrucache:libs/disklrucache.jar \
 include $(BUILD_MULTI_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MANIFEST_FILE := src/main/AndroidManifest.xml
-LOCAL_SRC_FILES := $(call all-java-files-under, src/main/java)
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/src/main/res
+LOCAL_SRC_FILES := $(call all-java-files-under, src/main/java) \
+                   $(call all-java-files-under, ../mp3id3library/src/main/java) \
+                   $(call all-java-files-under, ../ijkplayer/src/main/java) \
+                   $(call all-java-files-under, ../photoview/src/main/java) \
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/src/main/res \
+                      $(LOCAL_PATH)/../ijkplayer/src/main/res \
+                      $(LOCAL_PATH)/../../../../../../prebuilts/sdk/current/support/v7/recyclerview/res
 LOCAL_JAVA_LIBRARIES := framework
 LOCAL_STATIC_JAVA_LIBRARIES :=  glide-3.7.0 \
+                                disklrucache \
 								android-support-v4 \
-								android-support-v7-appcompat
+								android-support-v7-appcompat \
+								android-support-v7-recyclerview
+LOCAL_AAPT_FLAGS := \
+    --auto-add-overlay \
+    --extra-packages android.support.v7.recyclerview
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PACKAGE_NAME := JAC-MediaPlayers
 LOCAL_CERTIFICATE := platform
